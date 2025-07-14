c05bd200a2a2e8765c3df6bc5a5e0dc3e85b2ea0
Sachin230498 <sachinpathe230498@gmail.com>
2025-07-14 16:28:31 +0530

    js-2

---
+++ batch_3/javscript-1/prob1.js
@@ -0,0 +1,35 @@
+// varible = kind of container or box , where you can store the data and update or use when you needed
+
+// let var const
+
+// Data type 
+
+// String  =  whatever your wrote in " ", '' , "' '"
+// Number  =  full digit 7,2,10,5000
+// Boolean =  true/false
+
+let a = "Clothes"
+let b = "shoes"
+let c = "formal clothes"
+
+// console.log(a)
+
+// console.log(b)
+
+// console.log("hello",c,"my name is rohan")
+
+
+let x = 45;  //number
+let y = "11" // string
+
+// console.log(typeof x)
+
+
+let yes = true; //boolean
+// console.log(typeof yes)
+
+
+let abc = false;
+console.log(typeof abc)
+
+
