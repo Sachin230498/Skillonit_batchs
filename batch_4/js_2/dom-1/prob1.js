f22ff324a04359955581773c0cc8e1d1b61e7eb0
Sachin230498 <sachinpathe230498@gmail.com>
2025-08-18 12:53:11 +0530

    dom-1

---
+++ batch_4/js_2/dom-1/prob1.js
@@ -0,0 +1,41 @@
+// console.log("hello")
+// console.log("bye")
+
+// for(let i=1;i<=5;i++){
+//     console.log("Hii")
+// }
+
+// Dom = document object model
+// Dom is a technique where you can access html el or change in it using js
+
+// console.log(document.head)
+// console.log(document.images)
+
+
+// Event => Event is a action that is triggerd by user
+
+// onclick 
+
+
+function likeme(){
+    console.log("like me")
+}
+
+
+function abc(){
+    // console.log("bye")
+    alert("abcd")
+}
+// likeme()
+
+
+// document.getElementById(idname)
+
+let a = document.getElementById("head")
+
+// console.log(a.innerText)
+
+a.innerText = "Hello"
+
+let div = document.getElementById("main")
+// console.log(div.innerHTML)
