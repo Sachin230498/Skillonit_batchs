7cdc459859a64d5c22f9dd8da9219977fbabba0d
Sachin230498 <sachinpathe230498@gmail.com>
2026-01-13 17:29:28 +0530

    add econ

--- batch-5/js-2/LS/todo/index.js
+++ batch-5/js-2/LS/todo/index.js
@@ -8,7 +8,7 @@ form.addEventListener("submit", getData);
 let todo_arr = JSON.parse(localStorage.getItem("todo_arr")) || [];
 
 
-// display(todo_arr)
+display(todo_arr)
 
 function getData() {
   event.preventDefault();
@@ -32,8 +32,8 @@ display(todo_arr);
 
 function display(data){
     tbody.innerHTML = null
-//   console.log(data)
-data.map(function(el){
+
+data.map(function(el,index){
     let row = document.createElement("tr");
     let col1 = document.createElement("td");
     col1.innerText = el.task;
@@ -45,12 +45,26 @@ data.map(function(el){
           col2.style.backgroundColor = "green";
     }
     let col3 = document.createElement("td");
+
+    col3.addEventListener("click",function(){
+deletfun(index)
+    })
     col3.innerText = "Delete";
     col3.style.color = "white"
     col3.style.backgroundColor = "red"
     row.append(col1, col2, col3);
     tbody.append(row);
 })
+
+}
+
+
+let arr = [12,2,66,78]
+
+function deletfun(num){
+todo_arr.splice(num,1)
+localStorage.setItem("todo_arr", JSON.stringify(todo_arr));
+display(todo_arr)
 
 }
 
